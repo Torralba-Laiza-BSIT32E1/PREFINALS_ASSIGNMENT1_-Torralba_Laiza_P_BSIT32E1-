4cf9949488fc0a7a9f27463aaccd7646d3d03573
Torralba_Laiza_BSIT32E1 <160841939+Torralba-Laiza-BSIT32E1@users.noreply.github.com>
2024-06-30 17:45:48 +0800

    AUTH AND PROTECTED API

---
+++ PREFINALS_AUTH_TORRALBA_LAIZA-P/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_TORRALBA_LAIZA_P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
